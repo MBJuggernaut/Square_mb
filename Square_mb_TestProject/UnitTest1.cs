e737dcb527ee188e451e0182e8842af9444a8f4d
Марк Болотских <mbolotskikh@sdvor.com>
2023-01-13 23:32:25 +0300

    Добавил тестирование

---
+++ Square_mb_TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Square_mb_TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
